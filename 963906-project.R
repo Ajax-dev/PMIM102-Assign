593db2068f2e1ee9b76b6706cff35151dd9df214
Ajax-dev <ajwilt97@gmail.com>
2022-02-04 16:52:02 +0000

    First commit of the assignment for PMIM102J

---
+++ 963906-project.R
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------
+# 963906-project.R
+# Assignment_Wales_Data.Rproj
+# Version 1 - 04/02/2022
+# Alex Jones
+## This file is for the PMIM102J assignment for Scientific 
+## Computing In Healthcare, it will make use of R and SQL
+## to understand large datasets regarding health data in Wales
+## and gain various insights on this data based on user inputs
+# -----------------------------------------------------------
+
+#### Imports
+
+#### Setting constants
+
+#### Connecting to the database
+
+#### Taking user input
+
+#### Manipulation of data
+
+#### Visualization of data
